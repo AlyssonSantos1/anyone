d7953c4b2514c02999ad2faaa5d9f162207290db
AlyssonSantos1 <salysson280@gmail.com>
2025-02-06 11:32:59 -0300

    update

--- app/Http/Controllers/ExecutiveController.php
+++ app/Http/Controllers/ExecutiveController.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 
 class ExecutiveController extends Controller
 {
-    public function newmember (Request $request){
+    public function newmember (Request $request, hierarchy $hierarchy, id $id){
         $Squad = $request->squad;
         Squad::create([
             "name" =>$request->name_user,
@@ -17,6 +17,7 @@ class ExecutiveController extends Controller
 
         ]);
         //This function is add new users
+        // adicionar novos usuarios a empresa e so o executivo pode fazer isso por isso o request
 
         return view('New member has been increase');
     }
@@ -33,10 +34,11 @@ class ExecutiveController extends Controller
 
         ]);
         //This function is edit the users of the company
+        // essa funcao serve pra editar dados do usuario na empresa
         return view('the member has been edited');
     }
 
-    public function newproject (Request $request){
+    public function newproject (Request $request, hierarchy $hierarchy, id $id){
         $Squad = $request->squad;
         Squad::create([
             "projectname" =>$request->project_team,
@@ -48,43 +50,44 @@ class ExecutiveController extends Controller
 
         ]);
         //This function create new projects 
+        // essa funcao serve pra criar o projeto e so o executivo na hierarquia pode fazer isso por isso a request
 
         return view('New Project has been created');
     
     }
 
-    public function swapuser (Request $request, id $id, currentproject $currentproject){
+    public function swapuser (Request $request, id $id, hierarchy $hierarchy){
         $Squad = $request->squad;
         Squad::swapuser([
             "name" =>$request->name_user,
-            "currentproject" =>$request->currentproject_user
+            "currentproject" =>$request->currentproject_user,
+            "role" =>$request->role_user
 
         ]);
-        //This function is swap members among the projects 
+        //This function is swap members among the projects
+        // essa funcao pode trocar as funcoes dos usuarios na empresa ou trocar ele de projetos  
 
         return view('The User has been swaped team or role temporary');
     
     }
 
-    public function excludereview (Request $request){
+    public function excludereview (Request $request, hierarchy $hierarchy, id $id ){
         $Squad = $request->squad;
         Squad::delete([
-            "name" =>$request->name_user,
-            "email" =>$request->email_user,
-            "role" =>$request->role_user,
-            "hierarchy" =>$request->hierarchy_user,
-            "currentproject" =>$request->currentproject_user
+            "projectname" =>$request->projectname_project,
+            "reviews" =>$request->reviews_project,
 
         ]);
 
         return view('The Review Has been excluded');
+        //funcao que so executivo pode excluir comentarios mas nao podem editar
     
     }
 
-    public function vision (Request $request){
+    public function vision (Request $request, id $id, hierarchy $hierarchy){
         $Squad = $request->squad;
         Squad::read([
-            "projectname" =>$request->projectname_user,
+            "name" =>$request->name_user,
             "reviews" =>$request->currentproject_user
 
         ]);
@@ -93,34 +96,18 @@ class ExecutiveController extends Controller
     
     }
 
-    // public function acess (Request $request, id $id, hierarchy $hierarchy){
-    //     $Squad = $request->squad;
-    //     Squad::create([
-    //         "name" =>$request->name_user,
-    //         "email" =>$request->email_user,
-    //         "role" =>$request->role_user,
-    //         "hierarchy" =>$request->hierarchy_user,
-    //         "currentproject" =>$request->currentproject_user
-
-    //     ]);
-    //     //Function can permit the managers to acess the themselves reviews
-
-    //     return view('The manager now has acess to reviews of project and themselves');
-    // }
-
-    public function review (Request $request){
+    
+    public function review (Request $request, hierarchy $hierarchy){
         $Squad = $request->squad;
-        Squad::create([
+        Squad::read([
             "name" =>$request->name_user,
-            "email" =>$request->email_user,
-            "role" =>$request->role_user,
-            "hierarchy" =>$request->hierarchy_user,
-            "currentproject" =>$request->currentproject_user
+            "reviews" =>$request->reviews_project,
+            "projectname" =>$request->projectname_project           
 
         ]);
 
         return view('The manager now can see the review author name');
     }
-
+    //somente executivos podem ver o nome do autor das reviews
 
 }
