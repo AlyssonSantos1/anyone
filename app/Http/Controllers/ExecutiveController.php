2019726c08c71b85dc922fcb6a9059981c6fab39
AlyssonSantos1 <salysson280@gmail.com>
2025-02-08 17:50:43 -0300

    update

--- app/Http/Controllers/ExecutiveController.php
+++ app/Http/Controllers/ExecutiveController.php
@@ -6,43 +6,63 @@ use Illuminate\Http\Request;
 
 class ExecutiveController extends Controller
 {
-    public function newmember (Request $request, hierarchy $hierarchy, id $id){
-        $Squad = $request->squad;
-        Squad::create([
-            "name" =>$request->name_user,
-            "email" =>$request->email_user,
-            "role" =>$request->role_user,
-            "hierarchy" =>$request->hierarchy_user,
-            "currentproject" =>$request->currentproject_user
-
-        ]);
+    public function newmember (Request $request){
+        $name = $request->input('name');
+        $hierarchy = $request->input('hierarchy');
+        // validation for executive
         //This function is add new users
         // adicionar novos usuarios a empresa e so o executivo pode fazer isso por isso o request
         // aqui vai precisar do if pra saber se a pessoa é um executivo mesma logica if/else
-        // se nao a operacao nao pode ser gfeita e mostra uma mensagem "nao autorizado, so executivos" 
+        // se nao a operacao nao pode ser gfeita e mostra uma mensagem "nao autorizado, so executivos"
 
-        return view('New member has been increase');
+        if ($name == 'name' &&  $hierarchy == 'executive'){
+
+            Squad::create([
+                "name" =>$request->name_user,
+                "email" =>$request->email_user,
+                "role" =>$request->role_user,
+                "hierarchy" =>$request->hierarchy_user,
+                "currentproject" =>$request->currentproject_user
+    
+            ]);
+
+            return 'The operation is authorized you are an Executive';
+            
+        }else{
+
+            return 'No Permission Executive Only';
+
+        } 
+        return view('newproject');  
+
     }
 
     public function edit (Request $request, id $id){
-        $Squad = $request->squad;
-        Squad::edit([
-            "name" =>$request->name_user,
-            "email" =>$request->email_user,
-            "role" =>$request->role_user,
-            "hierarchy" =>$request->hierarchy_user,
-            "currentproject" =>$request->currentproject_user
-            
+        $name = $request->input('name');
+        $hierarchy = $request->input('hierarchy');
 
-        ]);
-        //This function is edit the users of the company
-        // essa funcao serve pra editar dados do usuario na empresa
-        // aqui vai precisar do if pra saber se a pessoa é um executivo mesma logica if/else
-        // se nao a operacao nao pode ser gfeita e mostra uma mensagem "nao autorizado, so executivos" 
-        return view('the member has been edited');
+        if ($name == 'name' &&  $hierarchy == 'executive'){
+
+            Squad::edit([
+                "name" =>$request->name_user,
+                "email" =>$request->email_user,
+                "role" =>$request->role_user,
+                "hierarchy" =>$request->hierarchy_user,
+                "currentproject" =>$request->currentproject_user
+                
+    
+            ]);
+
+        }else{
+
+            return 'No Permission to edit, Executive Only';
+
+        }          
+        
+        return view('editmember');
     }
 
-    public function newproject (Request $request, hierarchy $hierarchy, id $id){
+    public function newproject (Request $request){
         $Squad = $request->squad;
         Squad::create([
             "projectname" =>$request->projectname_project,
@@ -53,10 +73,7 @@ class ExecutiveController extends Controller
             "reviews" =>$request->reviews_project
 
         ]);
-        //This function create new projects 
         // essa funcao serve pra criar o projeto e so o executivo na hierarquia pode fazer isso por isso a request
-        // aqui vai precisar do if pra saber se a pessoa é um executivo mesma logica if/else
-        // se nao a operacao nao pode ser gfeita e mostra uma mensagem "nao autorizado, so executivos" 
         return view('New Project has been created');
     
     }
--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/executive/manager-add',[ExecutiveController::class, 'newmember']);
+Route::post('/executive/manager-add',[ExecutiveController::class, 'newmember'])->name('newmember');
+
 
