2ebda58dbf564cf39f1ae5a33e7755ac3bca5ebd
AlyssonSantos1 <salysson280@gmail.com>
2025-02-10 23:47:05 -0300

    update

--- routes/web.php
+++ routes/web.php
@@ -21,12 +21,12 @@ Route::get('/', function () {
 Route::get('/',[ExecutiveController::class, 'index']);
 // Route of Index
 
-Route::get('/executive/manager-add',[ExecutiveController::class, 'newmember']);
-Route::post('/executive/manager-add',[ExecutiveController::class, 'newmember'])->name('newuser');
-Route::get('/executive/manager-edit/{id}',[ExecutiveController::class, 'edit']);
-Route::put('/executive/manager-edit/{id}',[ExecutiveController::class, 'edit'])->name('memberedited');
-Route::get('/executive/manager-building',[ExecutiveController::class, 'newproject']);
-Route::post('/executive/manager-building',[ExecutiveController::class, 'newproject'])->name('buildproject');
+Route::get('/executive/add-users',[ExecutiveController::class, 'newmember']);
+Route::post('/executive/add-users',[ExecutiveController::class, 'newmember'])->name('newuser');
+Route::get('/executive/members-edit/{id}',[ExecutiveController::class, 'edit']);
+Route::put('/executive/members-edited/{id}',[ExecutiveController::class, 'edit'])->name('memberedited');
+Route::get('/executive/project-build',[ExecutiveController::class, 'newproject']);
+Route::post('/executive/project-build',[ExecutiveController::class, 'newproject'])->name('buildproject');
 Route::get('/executive/see-everything',[ExecutiveController::class, 'vision'])->name('vision360');
 // Routes Only can acess by Executives in the Company
 
